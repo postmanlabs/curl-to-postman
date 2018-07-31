8799b75d3d09942fbdb799996d8f2a4189428dfa
abhijitkane <abhijit@getpostman.com>
2018-07-31 18:26:50 +0530

    Adding separate convert.js

---
+++ src/convert.js
@@ -0,0 +1,22 @@
+var lib = require('./lib.js');
+
+module.exports = function (curlCommand, cb) {
+	var result = lib.convertCurlToRequest(curlCommand);
+    if(result.error) {
+        process.nextTick(function() {
+            cb(null, {
+                result: false,
+                reason: result.error.message
+            });
+        });
+    }
+    else {
+        process.nextTick(function() {
+            cb(null, {
+                result: true,
+                type: 'request',
+                data: result
+            });
+        });
+    }
+}
