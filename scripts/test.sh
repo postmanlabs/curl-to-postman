0ac8ff477fa30057d702cf7486dfd017bcf4ee07
Dhroov7 <dhroovgupta7@gmail.com>
2019-09-14 02:25:12 +0530

    removed unnecessary comments from test.sh file

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # ----------------------------------------------------------------------------------------------------------------------
-# This script is intended to be executed as all-inclusive test and artefact creation (excluding packaging and release.)
-# As such, a clean exit from this script implies all tests have passed and the code will not fail a build. (However,
-# build may still fail due to errors on build executing servers and as such has no relation with code quality.)
-#
 # This script executes all non time-bound tests, excluding browser tests and performance tests.
 # ----------------------------------------------------------------------------------------------------------------------
 
