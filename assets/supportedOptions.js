fc60da4dc9ae90cddb9ce1bb005d31b096435d57
shreys7 <shreyshah97@gmail.com>
2020-03-18 01:13:31 +0530

    Add list of supported args(options) in the curl converter, as a file in assets

---
+++ assets/supportedOptions.js
@@ -0,0 +1,31 @@
+// list of all supported options that are supported by this module
+
+let supportedOptions = [
+  '-V',
+  '--version',
+  '-A',
+  '--user-agent',
+  '-d',
+  '--data',
+  '--data-raw',
+  '--data-ascii',
+  '--data-urlencode',
+  '--data-binary',
+  '-F',
+  '--form',
+  '-G',
+  '--get',
+  '-H',
+  '--header',
+  '-X',
+  '--request',
+  '-I',
+  '--head',
+  '-T',
+  '--upload-file',
+  '--url',
+  '--basic',
+  '-u',
+  '--user'];
+
+module.exports = supportedOptions;
