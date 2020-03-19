6149da31bf8687aefb548f79a12f1081a360d3c1
shreys7 <shreyshah97@gmail.com>
2020-03-19 15:54:38 +0530

    Use supportedOptions array as a source of truth for commander options

--- assets/supportedOptions.js
+++ assets/supportedOptions.js
@@ -1,31 +1,101 @@
-// list of all supported options that are supported by this module
-
+// list of all options that are supported by this module
+/* eslint-disable max-len */
 let supportedOptions = [
-  '-V',
-  '--version',
-  '-A',
-  '--user-agent',
-  '-d',
-  '--data',
-  '--data-raw',
-  '--data-ascii',
-  '--data-urlencode',
-  '--data-binary',
-  '-F',
-  '--form',
-  '-G',
-  '--get',
-  '-H',
-  '--header',
-  '-X',
-  '--request',
-  '-I',
-  '--head',
-  '-T',
-  '--upload-file',
-  '--url',
-  '--basic',
-  '-u',
-  '--user'];
-
+  {
+    short: '-A',
+    long: '--user-agent',
+    description: 'An optional user-agent string',
+    format: '<string>',
+    collectValues: false
+  },
+  {
+    short: '-d',
+    long: '--data',
+    description: 'Sends the specified data to the server with type application/x-www-form-urlencoded. application/x-www-form-urlencoded',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    long: '--data-raw',
+    description: 'Sends the specified data to the server with type application/x-www-form-urlencoded. application/x-www-form-urlencoded',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    long: '--data-ascii',
+    description: 'Sends the specified data to the server with type application/x-www-form-urlencoded. application/x-www-form-urlencoded',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    long: '--data-urlencode',
+    description: 'Sends the specified data to the server with type application/x-www-form-urlencoded. application/x-www-form-urlencoded',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    long: '--data-binary',
+    description: 'Data sent as-is',
+    format: '[string]',
+    collectValues: false
+  },
+  {
+    short: '-F',
+    long: '--form',
+    description: 'A single form-data field',
+    format: '<name=content>',
+    collectValues: true
+  },
+  {
+    short: '-G',
+    long: '--get',
+    description: 'Forces the request to be sent as GET, with the --data parameters appended to the query string',
+    collectValues: false
+  },
+  {
+    short: '-H',
+    long: '--header',
+    description: 'Add a header (can be used multiple times)',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    short: '-X',
+    long: '--request',
+    description: 'Specify a custom request method to be used',
+    format: '[string]',
+    collectValues: false
+  },
+  {
+    short: '-I',
+    long: '--head',
+    description: 'Forces the request to be sent as HEAD, with the --data parameters appended to the query string',
+    collectValues: false
+  },
+  {
+    short: '-T',
+    long: '--upload-file',
+    description: 'Forces the request to be sent as PUT with the specified local file to the server',
+    format: '[string]',
+    collectValues: true
+  },
+  {
+    long: '--url',
+    description: 'An alternate way to specify the URL',
+    format: '[string]',
+    collectValues: false
+  },
+  {
+    long: '--basic',
+    description: 'Overrides previous auth settings',
+    collectValues: false
+  },
+  {
+    short: '-u',
+    long: '--user',
+    description: 'Basic auth ( -u <username:password>)',
+    format: '[string]',
+    collectValues: false
+  }
+];
 module.exports = supportedOptions;
