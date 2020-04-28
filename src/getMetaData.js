1ef3d3918ad0dd1dafa31cc811dc2c783db83521
umeshp7 <31915995+umeshp7@users.noreply.github.com>
2020-04-29 00:24:22 +0530

    add getMetaData function

---
+++ src/getMetaData.js
@@ -0,0 +1,37 @@
+var lib = require('./lib.js');
+
+module.exports = function (input, cb) {
+  var result;
+
+  if (!input) {
+    cb(null, {
+      result: false,
+      reason: 'Invalid input object provided'
+    });
+  }
+
+  if (input.type === 'string') {
+    result = lib.getMetaData(input.data);
+    if (result.error) {
+      return cb(null, {
+        result: false,
+        reason: result.error.message
+      });
+    }
+
+    return cb(null, {
+      result: true,
+      name: result.url,
+      output: [{
+        type: 'request',
+        data: result.url
+      }]
+    });
+
+  }
+
+  return cb(null, {
+    result: false,
+    reason: 'Only input.type=string supported for cURL'
+  });
+};
